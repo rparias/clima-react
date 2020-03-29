a063c05ee813d576d2e4d62a6675d0fa4738df08
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-29 14:39:31 -0500

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-      <h1>Clima React</h1>
+      <Header title="Clima React App" />
     </div>
   );
 }
