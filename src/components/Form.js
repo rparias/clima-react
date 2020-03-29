bdcb4403a18d4a90212c408877e7e5454398876a
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-29 17:26:57 -0500

    Working on onSubmit function

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,6 +5,7 @@ function Form() {
     city: '',
     country: ''
   });
+  const [error, updateError] = useState(false);
 
   const { city, country } = search;
 
@@ -15,8 +16,24 @@ function Form() {
     });
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (hasErrorData()) {
+      return;
+    }
+  };
+
+  const hasErrorData = () => {
+    const hasError = city === '' || country === '';
+    updateError(hasError);
+    return hasError;
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {error ? (
+        <p className="red darken-4 error">Todos los campos son obligatorios</p>
+      ) : null}
       <div className="input-field col s12">
         <input
           type="text"
@@ -44,6 +61,13 @@ function Form() {
           <option value="ES">España</option>
         </select>
         <label htmlFor="country">País</label>
+      </div>
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          value="Obtener Clima"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4"
+        />
       </div>
     </form>
   );
